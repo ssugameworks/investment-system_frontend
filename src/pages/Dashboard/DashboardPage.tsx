ea6022fef6990542b508dd1a770352a8744dc12c
Andy Hong <chili.tomat0@icloud.com>
2025-08-27 18:18:13 +0900

    chore: 대시보드 임시 페이지 생성

---
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -0,0 +1,10 @@
+function DashboardPage() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>투자 대시보드입니다.</p>
+    </div>
+  )
+}
+
+export default DashboardPage
--- src/pages/index.ts
+++ src/pages/index.ts
@@ -1,1 +1,2 @@
 export { default as LoginPage } from './Login'
+export { default as DashboardPage } from './Dashboard'
